3eb3eb373996ea4c4ded551847ba44df97dbe51f
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-12-26 11:47:53 +0100

    fixed the misstyped "d" mmlol

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ c.on("guildMemberAdd", (member) => {
 c.on("guildMemberUpdate",async (_,newMember) => {
     if (newMember.bot) return;
     if (newMember.roles.cache.size !== 0) return;
-    if (newMember.guild.ownerID === newMember.user.id) return;d
+    if (newMember.guild.ownerID === newMember.user.id) return;
     if (!newMember.guild.me.hasPermission("MANAGE_ROLES")) return;
     if (!c.db.get(`${newMember.guild.id}.config.roleAtJoin`)) return;
     if (newMember.guild.roles.cache.get(c.db.get(`${newMember.guild.id}.config.roleAtJoin`)).comparePositionTo(newMember.guild.me.roles.highest) >= 0) return;
